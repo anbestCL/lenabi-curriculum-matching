92405c91fefbb458c30c95919d3a8c846682b6a6
anbestCL <anna.bettina.steinberg@gmail.com>
2022-01-25 17:26:23 +0200

    subtopic added to query, partial match not working

--- matching.py
+++ matching.py
@@ -1,11 +1,24 @@
 import json
 import treetaggerwrapper
 import nltk 
+from nltk.corpus import stopwords
+
+nltk.download('stopwords')
+STOPWORDS = stopwords.words('german')
 TAGGER = treetaggerwrapper.TreeTagger(TAGLANG='de', TAGDIR='/Users/anbest/Downloads/tree-tagger-MacOSX-3.2.3/')
 
 def clean(heading_list):
+    # removing symbols
     words = [heading.split("(ca.")[0] for heading in heading_list]
     words = " ".join(words).split()
+    
+    # removing stopwords
+    words = [word for word in words if word not in STOPWORDS]
+
+    # remove duplicates
+    words = list(set(words))
+
+    # Funktionen -> Funktion
     words = lemmatize(words)
     return words
 
@@ -19,19 +32,20 @@ def get_headings(cur, level="top"):
         queries = clean(cur.keys())
     elif level == "subtopic":
         headings = []
-        for topic, subtopics in cur.keys():
+        for topic, subtopics in cur.items():
             headings.append(topic)
             for subtopic in subtopics:
                 headings.append(subtopic["name"])
-        queries = ",".join(headings).split()
+        queries = clean(headings)
+#        queries = ",".join(headings).split()
     return queries
 
 def find_matches(cur, query):
     matches = []
     for level in cur["children"]:
         for topic in level["children"]:
-            found = topic["name"].find(query)
-            if found != -1:
+            # partial match
+            if query in topic["name"]:
                 matches.append("&".join([level["name"],topic["name"]]))
 
     return matches
@@ -44,13 +58,16 @@ cur_bav = json.load(file_bav)
 file_sax = open("output/sachsen-mathematik.json")
 cur_sax = json.load(file_sax)
 
-
-queries = get_headings(cur_bav)
+queries = get_headings(cur_bav, level="subtopic")
 matches = {}
 for query in queries:
     matches[query] = find_matches(cur_sax, query)
 
-print(matches)
+for keyword in matches:
+    print(keyword)
+    print(matches[keyword])
+    print("\n")
+
 
 
 file_bav.close()
